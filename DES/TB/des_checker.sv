a7883f8ddfca281eed05d8eac0269a75d11a0a0e
Hoàng Bảo Long <105693455+106210049@users.noreply.github.com>
2026-07-27 15:10:51 +0700

    Add files via upload

--- DES/TB/des_checker.sv
+++ DES/TB/des_checker.sv
@@ -10,6 +10,18 @@ program des_checker(des_if vif);
     assert property (p_des_ready) 
     $display("[SVA][PASS] ASSERT DES READY");
     else $error("[SVA][FAIL] desc_ready assertion failed at time %0t", $time);
+    cover property (p_des_ready)
+        $display("[SVA][COVER] ASSERT DES READY");
 
-
+    property p_data_valid;
+        @(vif.clk) disable iff (!vif.rst_n)
+        $rose(vif.desc_ready)
+        |-> (vif.desc_result != 0);
+    endproperty
+    ASSERT_DATA_VALID:
+    assert property (p_data_valid)
+    $display("[SVA][PASS] ASSERT DATA VALID = %016h", vif.desc_result);
+    else $error("[SVA][FAIL] DATA MUST BE NON-ZERO");
+    cover property (p_data_valid)
+        $display("[SVA][COVER] ASSERT DATA VALID");
 endprogram
